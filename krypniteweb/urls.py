aa484ce83e18d12926b23c28457ac0ac3f96ae2c
Udrescu Vlad <udrescumvlad@gmail.com>
2021-09-01 17:36:39 +0300

    login and logout implemented

--- krypniteweb/urls.py
+++ krypniteweb/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path('products/', views.viewProducts, name='products'),
-]
+    path('login/', views.doLogin, name='login'),
+    path('register/', views.doRegister, name='register'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- krypniteweb/views.py
+++ krypniteweb/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,authenticate
+from django.contrib import messages
 from krypniteweb.templates import *
 
 # Create your views here.
 
 def viewProducts(request):
     return render(request, 'view_products.html')
+
+def doLogout(request):
+    if request.user.is_authenticated:
+        return redirect('login')
+
+def doLogin(request):
+    if request.user.is_authenticated:
+        return redirect('products')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, username+', welcome')
+                return redirect('products')
+            else:
+                messages.error(request, 'Username or password is incorrect.')
+                return render(request, 'login.html')
+        return render(request, 'login.html')
+
+def doRegister(request):
+    return render(request, 'register.html')
