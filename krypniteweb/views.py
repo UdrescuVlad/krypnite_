fae8a4cf5e49b66d0f2353a112d28aafdd2ad314
Udrescu Vlad <udrescumvlad@gmail.com>
2021-11-16 07:31:15 +0200

    more products are shown in rows and columnsand message error appear with red card box

--- krypniteweb/views.py
+++ krypniteweb/views.py
@@ -4,9 +4,9 @@ from django.contrib import messages
 from krypniteweb.models import Product
 from krypniteweb.forms import WishlistForm
 from krypniteweb.templates import *
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-
+@login_required
 def viewProducts(request):
     products = Product.objects.all()
     return render(request, 'view_products.html', {'products':products})
@@ -14,6 +14,7 @@ def viewProducts(request):
 def becomeMember(request):
     return render(request, 'become_member.html')
 
+@login_required
 def getWishlist(request):
     context = {}
     form = WishlistForm(request.POST)
@@ -28,20 +29,21 @@ def doLogout(request):
     return redirect('/krypnite/login')
 
 def doLogin(request):
+    context = {}
     if request.user.is_authenticated:
-        return redirect('/krypnite/products')
+        return redirect('/krypnite/products/')
     else:
         if request.method == 'POST':
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
+            username1 = request.POST.get('username')
+            password1 = request.POST.get('password')
+            user = authenticate(request, username=username1, password=password1)
             if user is not None:
                 login(request, user)
-                context = messages.success(request, username+', welcome')
-                return redirect('/krypnite/products', context)
+                messages.success(request, username1+', welcome')
+                return redirect('/krypnite/products/')
             else:
-                context = messages.error(request, 'Username or password is incorrect.')
-                return render(request, 'login.html', context)
+                messages.error(request, 'Username or password is incorrect.')
+                return render(request, 'login.html')
         return render(request, 'login.html')
 
 def doRegister(request):
