73dd93a68e9198cc674db936a3fb817c1bff5d72
Udrescu Vlad <udrescumvlad@gmail.com>
2021-11-25 18:30:01 +0200

    title will have link to specific detail page

--- krypniteweb/models.py
+++ krypniteweb/models.py
@@ -11,6 +11,8 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     description = models.TextField(max_length=200)
     product_photo = models.ImageField(null=True, blank=True, upload_to="images/")
+    def get_absolute_url(self):
+        return f"{self.slug}"
     def __str__(self):
         return self.product_name
 
--- krypniteweb/urls.py
+++ krypniteweb/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path('products/', views.viewProducts, name='products'),
     # re_path(r'^product/(?P<id>\d+)/', views.viewDetailedProduct, name='product'),
-    re_path(r'^product/(?P<slug>[\w-]+)/', views.viewDetailedProductBySlug, name='product'),
+    re_path(r'^products/(?P<slug>[\w-]+)/', views.viewDetailedProductBySlug, name='product'),
     path('login/', views.doLogin, name='login'),
     path('register/', views.doRegister, name='register'),
     path('logout/', views.doLogout, name='logout'),
