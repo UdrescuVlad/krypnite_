b2efb208b15ee071c9539bf7f4cacc189bb608da
Udrescu Vlad <udrescumvlad@gmail.com>
2021-12-01 08:40:40 +0200

    search bar is working

--- krypniteproject/settings.py
+++ krypniteproject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'krypniteweb',
+    'search_app',
     'crispy_forms'
 ]
 
--- search_app/views.py
+++ search_app/views.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from krypniteweb.models import Product
 
-# Create your views here.
+@login_required
+def searchViewByProduct(request):
+    print(request.GET)
+    get_q = request.GET.get('q', None)  # al 2-lea parametru este pentru valoarea default
+    print(get_q)
+    if get_q is not None:
+        query_set = Product.objects.filter(product_name__icontains=get_q)
+    else:
+        query_set = Product.objects.all()
+    
+    context={
+        'products':query_set
+    }
+    return render(request, 'view_products_on_search.html', context)
