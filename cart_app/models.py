7311e04302ba710e9125c5d3f85bc22fc0379830
Udrescu Vlad <udrescumvlad@gmail.com>
2021-12-09 19:48:33 +0200

    a cart home view is added

--- cart_app/models.py
+++ cart_app/models.py
@@ -1,10 +1,25 @@
 from django.db import models
 from django.conf import settings
 from krypniteweb.models import Product
+from django.db.models.signals import pre_save,m2m_changed
 
 class CartManager(models.Manager):
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        query = self.get_queryset().filter(id=cart_id)
+        if query.count() == 1:
+            new_obj = False
+            cart_object = query.first()
+            if request.user.is_authenticated and cart_object.user is None:
+                cart_object.user = request.user
+                cart_object.save()
+        else:
+            cart_object = Cart.objects.new(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_object.id
+        return cart_object, new_obj
+
     def new(self, user=None):
-        print(user)
         user_object = None
         if user is not None:
             if user.is_authenticated:
@@ -15,6 +30,7 @@ User = settings.AUTH_USER_MODEL
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
+    subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
@@ -23,3 +39,22 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+def cart_m2m_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        products_cart = instance.products.all()
+        total=0
+
+        for product in products_cart:
+            total = total + product.price
+        
+        instance.subtotal = total
+        
+        instance.save()
+m2m_changed.connect(cart_m2m_receiver, sender=Cart.products.through)
+
+#   I want that total to be also updated
+def cart_pre_save_receiver(sender, instance, *args, **kwargs):
+    if instance.total != instance.subtotal:
+        instance.total = instance.subtotal
+pre_save.connect(cart_pre_save_receiver, sender=Cart)
--- cart_app/views.py
+++ cart_app/views.py
@@ -2,17 +2,5 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart_home(request):
-
-    cart_id = request.session.get("cart_id", None)
-    print(cart_id)
-    query = Cart.objects.filter(id=cart_id)
-    if query.count() == 1:
-        cart_object = query.first()
-        if request.user.is_authenticated and cart_object.user is None:
-            cart_object.user = request.user
-            cart_object.save()
-    else:
-        cart_object = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_object.id
-    
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
     return render(request, "home.html", {})
