16d64d2c4840462d1e54998b479357dece20c510
Udrescu Vlad <udrescumvlad@gmail.com>
2021-12-19 16:40:02 +0200

    cart link added in the navbar

--- cart_app/urls.py
+++ cart_app/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    path('', views.cart_home, name='home'),
+    path('home', views.cart_home, name='home'),
     path('update', views.cart_update, name='update'),
 ] 
 
